9cfb8e70b46c94cabe91cf80af12bf53e796191a
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2022-10-29 19:12:30 +0530

    latest

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('get/',views.f1)
-    # path('reg-std/', views.regStudent),
-    # path('update-std/', views.updateStudent),
-    # path('delete-std/', views.delStudent),
-    # path('get-std/', views.getStudents),
+    path('reg-std/', views.regStudent),
+    path('update-std/', views.updateStudent),
+    path('delete-std/', views.delStudent),
+    path('get-std/', views.getStudents),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,54 @@
 # from django.shortcuts import render
+from rest_framework.decorators import api_view
+import json
+from .models import Students
+from rest_framework import status
 from rest_framework.response import Response
+#from myapp.serializers import StudentsSerializer
+from .serializers import StudentsSerializer
+# Create your views here.
 
-def f1(request):
-    return Response("sss")
+@api_view(["POST"])
+def regStudent(request):
+    try:
+        data = json.loads(request.body)
+        stdSerializer=StudentsSerializer(data=data)
+        if stdSerializer.is_valid():
+            stdSerializer.save()
+            return Response({"status":200,"msg":"success"}, status=status.HTTP_201_CREATED)
+        return Response("Not Inserted", status=status.HTTP_400_BAD_REQUEST)
+    except BaseException as e:
+        print(e)
+
+@api_view(["PUT"])
+def updateStudent(request):
+  try:
+     obj=Students.objects.get(id = request.headers["id"])
+     data = json.loads(request.body)
+     stdSerializer=StudentsSerializer(obj,data=data)
+     if stdSerializer.is_valid():
+          stdSerializer.save()
+          return Response("Success", status=status.HTTP_201_CREATED)
+     return Response("Not Updated", status=status.HTTP_400_BAD_REQUEST)
+  except BaseException as e:
+    print(e)
+
+@api_view(["GET"])
+def getStudents(request):
+    try:
+        students =  Students.objects.all().values()
+        serializer = StudentsSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    except BaseException as e:
+        print(e)
+        return Response(e)
+
+@api_view(["DELETE"])
+def delStudent(request):
+   try:
+     obj=Students.objects.get(id = request.GET["id"])
+     obj.delete()
+     return Response("Success", status=status.HTTP_201_CREATED)
+   except BaseException as e:
+     print(e)
+     return Response("Not Deleted", status=status.HTTP_400_BAD_REQUEST)
